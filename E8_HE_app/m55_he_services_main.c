31aed767e9dd4f395a750a4be445dc0779d6d15e
Prathima Uggappakodi <prathima.uggappakodi@alifsemi.com>
2025-08-27 17:03:33 -0700

    Committing the E8-HE main c file

--- E8_HE_app/m55_he_services_main.c
+++ E8_HE_app/m55_he_services_main.c
@@ -63,28 +63,10 @@
 #define CONTINUOUS_RUN         0
 #define LIMITED_RUN            1
 
-#define SE_MHU0_RECV_BASE      0x40040000
-#define SE_MHU0_SEND_BASE      0x40050000
-
-#define SE_MHU1_RECV_BASE      0x40060000
-#define SE_MHU1_SEND_BASE      0x40070000
-
-#define NUM_MHU                2
-
-#define MHU_M55_SE_MHU0        0
-#define MHU_M55_SE_MHU1        1
-
 #define MAXIMUM_TIMEOUT        0x01000000
 
 #define POWER_MSG_CNT       5
 
-/* CMSIS >= 0.9.0 names change */
-#if ALIF_CMSIS_BUILD_VERSION == 9
-#define MHU_SESS_S_RX_IRQ  MHU_SESS_S_RX_IRQ_IRQn
-#define MHU_SESS_S_TX_IRQ  MHU_SESS_S_TX_IRQ_IRQn
-#define MHU_SESS_NS_RX_IRQ MHU_SESS_NS_RX_IRQ_IRQn
-#define MHU_SESS_NS_TX_IRQ MHU_SESS_NS_TX_IRQ_IRQn
-#endif
 
 /*******************************************************************************
  *  T Y P E D E F S
@@ -94,20 +76,6 @@
  *  G L O B A L   V A R I A B L E S
  ******************************************************************************/
 
-uint32_t sender_base_address_list[NUM_MHU] =
-{
-  SE_MHU0_SEND_BASE,
-  SE_MHU1_SEND_BASE,
-};
-
-uint32_t receiver_base_address_list[NUM_MHU] =
-{
-  SE_MHU0_RECV_BASE,
-  SE_MHU1_RECV_BASE,
-};
-
-static mhu_driver_in_t s_mhu_driver_in;
-static mhu_driver_out_t s_mhu_driver_out;
 
 /* Data Required for Power Mode */
 static char   power_example_msg[POWER_MSG_CNT][64] =
@@ -129,86 +97,10 @@ static char   power_mode_msg[POWER_MSG_CNT][200] =
 };
 
 
-//extern void SERVICES_test(uint32_t services_handle);
-
-//extern uint32_t se_services_s_handle;
 /*******************************************************************************
  *  C O D E
  ******************************************************************************/
 
-/**
- * @brief MHU0 TX IRQ handler
- * @fn    void MHU_SESS_S_TX_IRQHandler(void)
- */
-/*void MHU_SESS_S_TX_IRQHandler(void)
-{
-  s_mhu_driver_out.sender_irq_handler(MHU_M55_SE_MHU0);
-}*/
-
-/**
- * @brief MHU0 RX IRQ handler
- * @fn    void MHU_SESS_S_RX_IRQHandler(void)
- */
-/*void MHU_SESS_S_RX_IRQHandler(void)
-{
-  s_mhu_driver_out.receiver_irq_handler(MHU_M55_SE_MHU0);
-}*/
-
-/**
- * @brief MHU1 TX IRQ handler
- * @fn    void MHU_SESS_NS_TX_IRQHandler(void)
- */
-/*void MHU_SESS_NS_TX_IRQHandler(void)
-{
-  s_mhu_driver_out.sender_irq_handler(MHU_M55_SE_MHU1);
-}*/
-
-/**
- * @fn    void MHU_SESS_NS_RX_IRQHandler(void)
- * @brief MHU1 RX IRQ handler
- */
-/*void MHU_SESS_NS_RX_IRQHandler(void)
-{
-  s_mhu_driver_out.receiver_irq_handler(MHU_M55_SE_MHU1);
-}*/
-
-/**
- * @brief Function to set up IRQs
- * @fn    static void setup_irq(int irq_num, uint32_t irq_handler)
- * @param irq_num
- * @param irq_handler
- */
-/*static void setup_irq(int irq_num, uint32_t irq_handler)
-{
-  NVIC_DisableIRQ(irq_num);
-  NVIC_ClearPendingIRQ(irq_num);
-  // Place the interrupt handler directly in the vector table (for XIP case)
-  //NVIC_SetVector(irq_num, irq_handler);
-  (void)(irq_handler);
-  NVIC_SetPriority(irq_num, 2);
-  NVIC_EnableIRQ(irq_num);
-}*/
-
-/**
- * @brief MHU initialize function
- * @fn    static void mhu_initialize(void)
- */
-/*static void mhu_initialize(void)
-{
-  s_mhu_driver_in.sender_base_address_list = sender_base_address_list;
-  s_mhu_driver_in.receiver_base_address_list = receiver_base_address_list;
-  s_mhu_driver_in.mhu_count = NUM_MHU;
-  s_mhu_driver_in.send_msg_acked_callback = SERVICES_send_msg_acked_callback;
-  s_mhu_driver_in.rx_msg_callback = SERVICES_rx_msg_callback;
-  s_mhu_driver_in.debug_print = SERVICES_print;
-  
-  MHU_driver_initialize(&s_mhu_driver_in, &s_mhu_driver_out);
-
-  setup_irq(MHU_SESS_S_RX_IRQ_IRQn, (uint32_t)&MHU_SESS_S_RX_IRQHandler);
-  setup_irq(MHU_SESS_S_TX_IRQ_IRQn, (uint32_t)&MHU_SESS_S_TX_IRQHandler);
-  setup_irq(MHU_SESS_NS_RX_IRQ_IRQn, (uint32_t)&MHU_SESS_NS_RX_IRQHandler);
-  setup_irq(MHU_SESS_NS_TX_IRQ_IRQn, (uint32_t)&MHU_SESS_NS_TX_IRQHandler);
-}*/
 
 /**
   @fn           void pm_usage_menu()
@@ -309,20 +201,6 @@ int main(void)
 
   
   SERVICES_print("[SE SERVICES] %s Test harness - STARTS\n", CPU_STRING);
-  
-  /**
-   * Initialize the MHU and SERVICES Library
-   */
-  //mhu_initialize();
-  //SERVICES_Setup(s_mhu_driver_out.send_message, MAXIMUM_TIMEOUT);
-  
-  //SERVICES_wait_ms(0x1000000);
-
-  //uint32_t services_handle = SERVICES_register_channel(MHU_M55_SE_MHU0, 0);
-
-  /**
-   * Call the SERVICES test harness
-   */
 
    se_services_port_init();
 
@@ -391,6 +269,7 @@ int main(void)
     if((bk_ram_wr(&hp_start_flag,START_OFFSET))!= -1)
     break;
   }
+  *(volatile uint32_t*)0x1A010400 = 4;
 
   switch(power_type)
   {
